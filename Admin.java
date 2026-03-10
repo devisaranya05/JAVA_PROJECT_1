6221cb36bcca14080269b4bc3fc2046ff2ca236b
Sambattula Devi Saranya <devisaranya5558@gmail.com>
2026-03-10 14:25:44 +0530

    Admin.java

---
+++ Admin.java
@@ -0,0 +1,40 @@
+package comm.coursereg.model;
+
+public class Admin {
+
+    private int adminId;
+    private String adminName;
+    private String password;
+
+    public Admin() {}
+
+    public Admin(int adminId, String adminName, String password) {
+        this.adminId = adminId;
+        this.adminName = adminName;
+        this.password = password;
+    }
+
+    public int getAdminId() {
+        return adminId;
+    }
+
+    public void setAdminId(int adminId) {
+        this.adminId = adminId;
+    }
+
+    public String getAdminName() {
+        return adminName;
+    }
+
+    public void setAdminName(String adminName) {
+        this.adminName = adminName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
